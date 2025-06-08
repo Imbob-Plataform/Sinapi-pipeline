8fdab98c0c4f3f128eaece4b8baf1addafbfb768
deezinn <developer.andrelz@gmail.com>
2025-06-08 16:43:20 -0300

    feat(config) new config

--- src/jobs/sinapi_job.py
+++ src/jobs/sinapi_job.py
@@ -6,9 +6,9 @@ class PipelineSinapi:
       self.extract = HttpSinapi.from_json('api.json')
       if self.extract is None:
          raise ValueError("Falha ao carregar configuração do arquivo 'api.json'")
-      
+
    def run_pipeline(self):
-      data = self.extract.get_http_sinapi()
+      data = self.extract.get_http_sinapi()  # type: ignore
       if not data or data is None:
          print("Falha ao obter dados da api")
          return
